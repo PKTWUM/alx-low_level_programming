28d36f2f6eecbda114d1512ac757413c4f2c6333
PKTWUM <pbtwum@st.ug.edu.gh>
2023-03-16 15:18:02 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{	
+	printf(*\*Programming is like building a multilingual puzzle");
+	           return (0);
+}		   
