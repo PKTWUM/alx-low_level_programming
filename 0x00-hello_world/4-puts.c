4df36a7735d73729ffd259fbf36050dfee4520de
PKTWUM <pbtwum@st.ug.edu.gh>
2023-03-16 15:22:55 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 /**
  * main - entry point
@@ -6,6 +5,6 @@
  */
 int main (void)
 {	
-	printf(*\*Programming is like building a multilingual puzzle");
+	puts(*\*Programming is like building a multilingual puzzle");
 	           return (0);
 }		   
