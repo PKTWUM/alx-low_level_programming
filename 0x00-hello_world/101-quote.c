914aa0d2f28956de4d727963f3e680625429a4e4
PKTWUM <pbtwum@st.ug.edu.gh>
2023-03-16 17:48:34 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,8 @@
  * main - Entry point
  * Return: Always 1 (Success)
  */
-* int main(void)
-*{
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
 return (1);
+}
