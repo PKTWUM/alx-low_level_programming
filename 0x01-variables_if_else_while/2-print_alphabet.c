0ab1dae0c72064b0f8dbef231c63b6dd2713faaf
PKTWUM <pbtwum@st.ug.edu.gh>
2023-03-17 17:58:40 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
-/**
- * main -Entry point
- * Return: always 0
- */
-int main (void)
-{
-	int n = 97;
 
-	while (n <= 122)
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
 	{
-		putchar(n);
-	       	n++;
+		putchar(alp[i]);
 	}
 	putchar('\n');
 	return (0);
+}
 
-	
