15edb40270007c4fe30c3f6c1e93aad324ea2d4a
PKTWUM <pbtwum@st.ug.edu.gh>
2023-03-17 18:06:27 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,30 +1,21 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-/**
-*main - Entry point
-*Description: 'check for number if last number is 16'
-*Return: always 0
-*/
-int main(void)
-{     int n;
-      int ld;
 
-       srand(time(0));
-       n = rand() - RAND_MAX / 2;
-       ld = n % 10;
-       if (ld > 5)
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
 	{
-	     printf("last digit of %d is %d and is grater than 5\n", n, ld);
+		putchar(alp[i]);
 	}
-	else if (ld == 0)
-	{
-		printf("last digit of %d is %d and is not 0\n", n, ld);
-	}
-	else
-	{
-		printf("last digit of %d is %d and less than 6 and not 0\n", n, ld);
-	}
+	putchar('\n');
 	return (0);
 }
 
