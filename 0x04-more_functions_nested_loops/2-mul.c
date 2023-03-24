c7f5709c156ae00de416947355674fb603854d51
PKTWUM <pbtwum@st.ug.edu.gh>
2023-03-23 22:32:46 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  * mul - multiply 2 int
- * oc: first int
- * oc: second int
+ * @a: first int
+ * @b: second int
  * Return: addition of the multipliers
  */
-int mul (int a. int b)
+int mul(int a, int b)
 {
-	return (a*b)
+	return (a * b);
 }
