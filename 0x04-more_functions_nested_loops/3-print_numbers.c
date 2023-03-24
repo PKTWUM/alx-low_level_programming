335372f53c821ab6f6d297b9d9ef60c451256213
PKTWUM <pbtwum@st.ug.edu.gh>
2023-03-23 23:01:02 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * print_numbers - print 0 to 9
  * Return void
@@ -12,5 +11,5 @@ void print_numbers(void)
 	{
 		_putchar(c);
 	}
-		_putchar('\n');
+	_putchar('\n');
 }
