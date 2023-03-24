3a91fe22c91e11924b44e9e697ca3af55003bac1
PKTWUM <pbtwum@st.ug.edu.gh>
2023-03-23 22:17:21 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * is digit - check if nos are 0-9
- * oc: 
+ * _isdigit - check if nos are 0-9
+ * @c: print numbers between 0 to 9
  *
  * Return: 0 or 1
  */
-int_isdigit(int c)
+int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
 		return (1);
